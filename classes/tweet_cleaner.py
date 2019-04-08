7edae52f2504cb301c0cea8c5c45e4a512dbbdb4
kamisiuu <kamil@hotmail.no>
2019-04-08 20:32:32 +0200

    working tweet_cleaner

--- classes/tweet_cleaner.py
+++ classes/tweet_cleaner.py
@@ -1,34 +1,33 @@
 import pandas as pd
 import numpy as np
 import nltk
+nltk.download('wordnet')
 from nltk.corpus import stopwords
 nltk.download('stopwords')
 from nltk.stem.porter import *
-
-tokenized_tweet = []
-
+from textblob import Word
 
 
-def noise(data,tweetcolumn):
+def noise(data, tweetcolumn):
     """ @Author = Kamil Lipski #
     noise () function  removes special characters, numbers, punctuations and removes twitter handles (@user)
-
     :param data: you have to give it a dataset
     :param tweetcolumn: you have to specify the column name that you what to remove noise from
     :return: it returns the preprocessed dataset
     """
+
     def remove_pattern(input_txt, pattern):
         r = re.findall(pattern, input_txt)
         for i in r:
             input_txt = re.sub(i, '', input_txt)
         return input_txt
+
     data[tweetcolumn] = data[tweetcolumn].str.replace("[^a-zA-Z#]", " ")
     data[tweetcolumn] = np.vectorize(remove_pattern)(data[tweetcolumn], "@[\w]*")
     return data
 
 
-
-def short_words(data,tweetcolumn):
+def short_words(data, tweetcolumn):
     """ @Author = Kamil Lipski #
     short_words() function removes all short words from dataset
 
@@ -41,8 +40,7 @@ def short_words(data,tweetcolumn):
     return data
 
 
-
-def stop_words(data,tweetcolumn):
+def stop_words(data, tweetcolumn):
     """@Author = Kamil Lipski #
     stop_words () function removes commonly occurring words from the text data.
 
@@ -52,11 +50,11 @@ def stop_words(data,tweetcolumn):
     """
     stop = stopwords.words('english')
     data[tweetcolumn] = data[tweetcolumn].apply(lambda x: " ".join(x for x in x.split() if x not in stop))
+    print("stop words")
     return data
 
 
-
-def rare_words(data,tweetcolumn):
+def rare_words(data, tweetcolumn):
     """@Author = Kamil Lipski #
     rare_words () function removes rare occurring words from the text data.
 
@@ -70,8 +68,35 @@ def rare_words(data,tweetcolumn):
     return data
 
 
+def common_words(data, tweetcolumn):
+    # Common word removal
+    freq = pd.Series(' '.join(data[tweetcolumn]).split()).value_counts()[:10]
+    freq = list(freq.index)
+    data[tweetcolumn] = data[tweetcolumn].apply(lambda x: " ".join(x for x in x.split() if x not in freq))
+    return data
 
-def lower_case(data,tweetcolumn):
+
+def tokenization(data, tweetcolumn):
+    data = data[tweetcolumn].apply(lambda x: x.split())
+    return data
+
+
+def stemming(data, tweetcolumn):
+    stemmer = PorterStemmer()
+    tokenized_tweet = tokenization(data, tweetcolumn)
+    tokenized_tweet.apply(lambda x: [stemmer.stem(i) for i in x])  # stemming
+
+    for i in range(len(tokenized_tweet)):
+        tokenized_tweet[i] = ' '.join(tokenized_tweet[i])
+    return data
+
+
+def lemmatization(data, tweetcolumn):
+    data[tweetcolumn] = data[tweetcolumn].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
+    return data
+
+
+def lower_case(data, tweetcolumn):
     """@Author = Kamil Lipski #
     lower_case () function transoforms words from the text data to lower case.
 
@@ -81,15 +106,25 @@ def lower_case(data,tweetcolumn):
     """
     data[tweetcolumn] = data[tweetcolumn].apply(lambda x: " ".join(x.lower() for x in x.split()))
     return data
-def datareturn(data):
-    return data
+
+
 class tweet_cleaner:
-    """This is a constructor of class tweet_cleaner
+    """This is a constructor of class tweet_cleaner that has return
         @Author=Kamil Lipski
     You create this class in following way:
        data = tweet_cleaner(dataset,columnwithtweets,cleaningoptions=['stop_words','rare_words'])
     """
-    def __new__(self,data,tweetcolumn, preprocessoptions=[]):
+    global choiceList,data
+    choiceList = {'noise': 'noise(cls.data, cls.tweetcolumn)',
+                  'short_words': 'short_words(cls.data, cls.tweetcolumn)',
+                  'stop_words': 'stop_words(cls.data, cls.tweetcolumn)',
+                  'rare_words': 'rare_words(cls.data, cls.tweetcolumn)',
+                  'common_words': 'common_words(cls.data, cls.tweetcolumn)',
+                  'tokenization': 'tokenization(cls.data, cls.tweetcolumn)',
+                  'stemming': 'stemming(cls.data, cls.tweetcolumn)',
+                  'lemmatization': 'lemmatization(cls.data, cls.tweetcolumn)',
+                  'lower_case': 'lower_case(cls.data, cls.tweetcolumn)'}
+    def __new__(cls, data, tweetcolumn, preprocessoptions=[]):
         """
                @Author=Kamil Lipski # This is a class constructor of class tweet_cleaner
 
@@ -98,24 +133,14 @@ class tweet_cleaner:
                :param preprocessoptions: 'noise'-removal of special characters,numbers,punctutation, twitter handles # 'short_words'-removal of short words #'stop_words'- removal of stop words #'rare_words'- removal of rare words#'lower_case' - transforms words into lower case #'tokenization' - transforms text into vectors #'stemming' - stemming is a rule-based process of stripping the suffixes (“ing”, “ly”, “es”, “s” etc) from a word. For example, For example – “play”, “player”, “played”, “plays” and “playing” are the different variations of the word – “play”.
                :return: returns dataset after preprocessing
                """
-        self.data = data
-        self.tweetcolumn = tweetcolumn
-        self.preprocessoptions = preprocessoptions  # this gives
-        choiceList = {'noise': noise(data, tweetcolumn),
-                      'short_words': short_words(data, tweetcolumn),
-                      'stop_words': stop_words(data, tweetcolumn),
-                      'rare_words': rare_words(data, tweetcolumn),
-                      'lower_case': lower_case(data, tweetcolumn)}
-        for option in preprocessoptions:
-            self.data = choiceList.get(option)
-        return self.data
+        cls.data = data
+        cls.tweetcolumn= tweetcolumn
+        cls.preprocessoptions = preprocessoptions  # this gives
 
-
-
-def clean_data(train,train_tweet):
-    # this function helps to remove twitter handles , twitter data contain lots of twitter handles like "@username",
-    # we remove those signs and usernames, because we dont won't to store peoples information, and at the same time this type of information
-    # is not needed for our tests
+        for option in cls.preprocessoptions:
+            mycode= choiceList.get(option)
+            cls.data = exec(mycode)
+        return data
 
 
 
@@ -124,34 +149,6 @@ def clean_data(train,train_tweet):
 
 
 
-
-
-    # Common word removal
-    freq = pd.Series(' '.join(train[train_tweet]).split()).value_counts()[:10]
-    freq = list(freq.index)
-    train[train_tweet] = train[train_tweet].apply(lambda x: " ".join(x for x in x.split() if x not in freq))
-
-    # # Spelling correction
-    # from textblob import TextBlob
-    # train[train_tweet].apply(lambda x: str(TextBlob(x).correct()))
-
-    # Tokenization process of building a dictionary and transform document into vectors
-    tokenized_tweet = train[train_tweet].apply(lambda x: x.split())
-
-
-    # Stemming is a rule-based process of stripping the suffixes (“ing”, “ly”, “es”, “s” etc)
-    # from a word. For example, For example – “play”, “player”, “played”, “plays” and “playing” are the
-    # different variations of the word – “play”.
-
-    stemmer = PorterStemmer()
-    tokenized_tweet = tokenized_tweet.apply(lambda x: [stemmer.stem(i) for i in x]) # stemming
-
-
-    for i in range(len(tokenized_tweet)):
-        tokenized_tweet[i] = ' '.join(tokenized_tweet[i])
-
-    train[train_tweet] = tokenized_tweet
-    return train
 
 
 
--- tests.py
+++ tests.py
@@ -7,7 +7,34 @@ train = pd.read_csv("data/dataset_1/train.csv", header='infer', index_col=None)
 
 from classes.tweet_cleaner import tweet_cleaner
 #data = pd.read_csv('data/results/accuracy_table/all_results_from_training.csv')
-print(train)
 
-data = tweet_cleaner(train,'tweet',preprocessoptions=['noise','short_words'])
+
+data = tweet_cleaner(train,'tweet',preprocessoptions=['noise'])
 print(data)
+
+
+# global listen
+# listen= [1,2,3,4]
+# def fun(variable):
+#     listen.remove(variable)
+#
+# class Test:
+#     global choicesList
+#     choicesList = {'a': 'fun(2)',
+#             'b':'fun(1)',
+#             'c':'fun(3)'}
+#     def __new__(cls,arry=[]):
+#         cls.arry=arry
+#
+#         for element in cls.arry:
+#             mycode = choicesList.get(element)
+#             exec(mycode)
+#
+# tt= Test(arry=['a','b'])
+# print(listen)
+
+
+
+
+
+
